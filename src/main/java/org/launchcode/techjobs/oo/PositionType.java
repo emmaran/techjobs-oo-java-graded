3def886a0deadc3fe998c9f442d7c3baaccd8b9b
emmaran <emmalrandolph@gmail.com>
2022-09-17 01:44:29 -0500

    Included override of 'hashCode' in 'PositionType' class

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -1,5 +1,7 @@
 package org.launchcode.techjobs.oo;
 
+import java.util.Objects;
+
 public class PositionType {
 
     private int id;
@@ -35,6 +37,11 @@ public class PositionType {
         if (!(o instanceof PositionType)) return false;
         PositionType positionType = (PositionType) o;
         return getId() == positionType.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, value);
     }
 
     // Getters and Setters:
